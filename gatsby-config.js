31ae71e345ae4d8bcc13c606ae9bb6219f354153
maanizfar <maanizfar@gmail.com>
2020-11-10 15:23:14 +0500

    clean code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Jamstack Todo`,
+    description: `A Todo app built with GatsbyJS, FaunaDB and Netlify.`,
+    author: `@maanizfar`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
